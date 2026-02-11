cf7b255bdd285c3061418b0278417959b11a3a60
Mikashiya <lemonovamikashiya@gmail.com>
2026-02-11 17:58:48 +0700

    Changes to be committed: 	modified:   index.php 	modified:   style/output.css
    Changes not staged for commit:
    	modified:   index.php
    	modified:   style/output.css
    
    Untracked files:
    	img/menu.png

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="style/output.css">
 </head>
 <body class="antialiased">
-    <header id="header" class="w-full h-15 bg-white md:flex grid items-center px-5 md:gap-10 shadow-md transition-all overflow-hidden">
+    <header id="header" class="fixed z-10 w-full h-15 bg-white md:flex grid items-center px-5 md:gap-10 shadow-md transition-all overflow-hidden">
         <div class="h-15 flex">
             <img src="img/RESTOQR.png" alt="Resto Logo">
             <div class="flex items-center justify-end md:hidden w-full">
@@ -20,11 +20,64 @@
             </div>
         </div>
         <ul class="md:flex items-center gap-5 space-y-3 md:space-y-0 bg-white w-full">
-            <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Menu Spesial</a></li>
-            <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Makanan</a></li>
-            <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Minuman</a></li>
+            <li><a href="#menuspesial" class="text-zinc-800 hover:opacity-70 transition-all">Menu Spesial</a></li>
+            <li><a href="#makanan" class="text-zinc-800 hover:opacity-70 transition-all">Makanan</a></li>
+            <li><a href="#minuman" class="text-zinc-800 hover:opacity-70 transition-all">Minuman</a></li>
         </ul>
     </header>
+    <main class="pt-19 px-4 pb-4 space-y-5">
+        <div id="menuspesial">
+            <p class="font-xl text-zinc-800 font-semibold">Menu Spesial</p>
+            <div class="size-full grid grid-cols-2 md:grid-cols-4 gap-3">
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+            </div>
+        </div>
+        <div id="makanan">
+            <p class="font-xl text-zinc-800 font-semibold">Makanan</p>
+            <div class="size-full grid grid-cols-2 md:grid-cols-4 gap-3">
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+            </div>
+        </div>
+        <div id="minuman">
+            <p class="font-xl text-zinc-800 font-semibold">Minuman</p>
+            <div class="size-full grid grid-cols-2 md:grid-cols-4 gap-3">
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+                <div class="col-span-1 rounded-md shadow-md h-50 md:h-150">
+
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 <script>
     function toggleHeader() {
