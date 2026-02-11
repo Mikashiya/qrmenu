00b8801a02f62104c151b28ccc33bcfc63f2daed
Mikashiya <lemonovamikashiya@gmail.com>
2026-02-11 17:24:20 +0700

    new file:   img/RESTOQR.png 	modified:   index.php 	modified:   style/output.css

--- index.php
+++ index.php
@@ -11,12 +11,15 @@
     <!-- Tailwindcss -->
     <link rel="stylesheet" href="style/output.css">
 </head>
-<body class="antialiased bg-white">
-    <header id="header" class="w-full h-15 bg-white grid items-center shadow-md transition-all overflow-hidden">
-        <div class="h-15 flex items-center justify-end px-5 md:hidden w-full">
-            <button onclick="toggleHeader()"><i class="bi bi-list text-zinc-800 text-xl"></i></button>
+<body class="antialiased">
+    <header id="header" class="w-full h-15 bg-white md:flex grid items-center px-5 md:gap-10 shadow-md transition-all overflow-hidden">
+        <div class="h-15 flex">
+            <img src="img/RESTOQR.png" alt="Resto Logo">
+            <div class="flex items-center justify-end md:hidden w-full">
+                <button onclick="toggleHeader()"><i class="bi bi-list text-zinc-800 text-xl"></i></button>
+            </div>
         </div>
-        <ul class="md:flex items-center gap-3 space-y-3 md:space-y-0 bg-white w-full px-5">
+        <ul class="md:flex items-center gap-5 space-y-3 md:space-y-0 bg-white w-full">
             <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Menu Spesial</a></li>
             <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Makanan</a></li>
             <li><a href="" class="text-zinc-800 hover:opacity-70 transition-all">Minuman</a></li>
@@ -26,7 +29,7 @@
 <script>
     function toggleHeader() {
         const header = document.getElementById("header");
-        header.classList.toggle("h-45");
+        header.classList.toggle("h-50");
         header.classList.toggle("items-center");
     }
 </script>
