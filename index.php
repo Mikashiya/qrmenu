5a0b3cf99d3cf2fc120370fda00149d1bbeb9ef1
Mikashiya <lemonovamikashiya@gmail.com>
2026-02-11 15:07:07 +0700

    modified:   index.php 	new file:   style/input.css 	new file:   style/output.css

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>QR Menu</title>
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+
+    <!-- Tailwindcss -->
+    <link rel="stylesheet" href="style/output.css">
 </head>
-<body>
+<body class="antialiased bg-white">
     
 </body>
 </html>
